d40108b8969b779552292621a5d9d1e1ed5d85d7
Gabx04 <gabriela.segurasaune@gmail.com>
2016-04-04 17:49:00 -0500

    contact y home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,12 @@
-$('.responsive').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 6,
-  slidesToScroll: 6,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 6,
-        slidesToScroll: 6,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
+(function(window, document, $){
+  $(document).ready(function(){
+    $('slider').slick({
+      slidesToShow: 6,
+      preArrow: '.slider-container .prev',
+      nextArrow: '.slider-container .next';
+    })
+  });
+})(document, window, jQuery);
+
+
+
